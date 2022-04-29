d5a27d7ac3ca8b4be24b2c29301cc0e84f5b4215
jihye615 <ghjs618@gmail.com>
2022-04-29 17:04:01 +0900

    first commit

---
+++ src/Theme.js
@@ -0,0 +1,15 @@
+export const lightTheme ={
+
+    bgColor : "#fff",
+    textColor : "#000",
+    headerColor : "#000",
+    headerTextColor : "#fff"
+}
+
+export const darkTheme ={
+
+    bgColor : "#000",
+    textColor : "#fff",
+    headerColor : "#fff",
+    headerTextColor : "#000"
+}
